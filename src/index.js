b8c39938eebe1085d59c6a8578cc069378644c60
Mikel Jagan <mikelmnj@gmail.com>
2022-03-10 13:32:22 -0500

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+// import { AppProvider } from './store';
+import App from 'scenes/App/App';
+import '@fortawesome/fontawesome-pro/css/all.css';
+import './index.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const MyApp = (
+  <StrictMode>
+    {/* <AppProvider> */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    {/* </AppProvider> */}
+  </StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const target = document.querySelector('#root');
+
+// React 18
+// ReactDOM.createRoot(target).render(MyApp);
+
+// React 17
+ReactDOM.render(MyApp, target);
