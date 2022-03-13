0631bc211bf2e87ea8b926a863a902b486b90372
Mikel Jagan <mikelmnj@gmail.com>
2022-03-13 10:00:42 -0400

    Added test route.

---
+++ functions/routes/test.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const router = express.Router();
+require('dotenv').config();
+
+// @route   GET api/v1/test
+// @desc    API test response endpoint.
+// @access  Public
+router.get('/test', async (req, res) => {
+  try {
+    res.json({
+      message: "API response received.",
+      type: "success"
+    });
+  } catch(err) {
+    res.status(500).send('Server error.');
+  }
+});
+
+module.exports = router;
