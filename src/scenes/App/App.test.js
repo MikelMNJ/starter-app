430dfa949c5e2b4099cd5ac5cec37a40206df57d
Mikel Jagan <8027678+MikelMNJ@users.noreply.github.com>
2022-03-16 23:08:28 -0400

    Removed incomplete test file.

--- src/scenes/App/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('Renders sample component.', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Component/i);
-  expect(linkElement).toBeInTheDocument();
-});
