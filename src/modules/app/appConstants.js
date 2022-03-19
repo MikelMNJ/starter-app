e8b647c680df79ed7aca7899eedc7746649cf9a4
Mikel Jagan <mikelmnj@gmail.com>
2022-03-19 17:57:09 -0400

    Added email API call to front-end.

--- src/modules/app/appConstants.js
+++ src/modules/app/appConstants.js
@@ -4,12 +4,14 @@ const constants = {
   SAMPLE_ACTION: "modules/app/SAMPLE_ACTION",
   ADD_NOTIFICATION: "modules/app/ADD_NOTIFICATION",
   REMOVE_NOTIFICATION: "modules/app/REMOVE_NOTIFICATION",
+  SEND_EMAIL: "modules/app/SEND_EMAIL",
   SAMPLE_API_CALL: "modules/app/SAMPLE_API_CALL",
   TEST_RATE_LIMIT: "modules/app/TEST_RATE_LIMIT",
 
   // Selectors
   STATE_KEY_SAMPLE_SELECTOR: "sampleSelector",
   STATE_KEY_NOTIFICATIONS: "notifications",
+  STATE_KEY_EMAIL_RESPONSE: "emailResponse",
   STATE_KEY_SAMPLE_API_RESPONSE: "sampleAPIResponse",
 };
 
