8103f71463e4cdef43232a43ebb7fadf1936a911
Mikel Jagan <mikelmnj@gmail.com>
2022-03-19 18:05:39 -0400

    Adjusted rate limit on email requests.

--- functions/routes/emailRoutes.js
+++ functions/routes/emailRoutes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const limiter = require('../middleware/limitMiddleware');
 
 const {
+  checkEmailPayload,
   sendEmail,
 } = require('../controllers/emailController');
 
 // Email
 router.route('/')
-  .post(limiter(), sendEmail);
+  .post(limiter(1, 5000), checkEmailPayload, sendEmail);
 
 module.exports = router;
