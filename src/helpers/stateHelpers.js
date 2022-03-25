9ead1dba440be5b7a27df17a29c0d417404f5753
Mikel Jagan <mikelmnj@gmail.com>
2022-03-25 12:24:19 -0400

    Added .merge() method with recursive array support.
    No object support yet.

--- src/helpers/stateHelpers.js
+++ src/helpers/stateHelpers.js
@@ -1,6 +1,6 @@
 import { useEffect, useReducer, useRef } from 'react';
 import { useStore } from 'store';
-import { isArray, isEmpty, isObject } from 'lodash';
+import { isArray, isEmpty, isObject, isEqual } from 'lodash';
 import { objectTypeError, objectKeyError, targetError } from 'errors/stateErrors';
 
 export const actionCreator = (type, payload, meta) => {
@@ -66,6 +66,132 @@ class StateManager {
   constructor(initialState, action) {
     this.initialState = initialState;
     this.action = action;
+  };
+
+  merge(array) {
+    const changed = [];
+    const added = [];
+    const removed = [];
+    let workingState = { ...this.initialState };
+
+    if (!array) console.warn("No merge array provided, state unchanged.");
+
+    if (!isEmpty(array)) {
+      const changedKeys = modifiedState => {
+        return Object.keys(workingState).map(key => {
+          const prev = workingState[key];
+          const current = modifiedState[key];
+          const bothPresent = prev && current;
+          const hasChange = !isEqual(prev, current);
+
+          if (bothPresent && hasChange) changed.push({ [key]: current });
+          return null;
+        });
+      };
+
+      const addedKeys = modifiedState => {
+        Object.keys(modifiedState).map(key => {
+          const prev = workingState[key];
+          const current = modifiedState[key];
+          if (!prev && current) added.push({ [key]: current });
+          return null;
+        });
+      };
+
+      const removedKeys = modifiedState => {
+        Object.keys(workingState).map(key => {
+          const justAdded = added.includes(key);
+          if (!justAdded && !modifiedState[key]) removed.push(key);
+          return null;
+        });
+      };
+
+      // Record changes for all main keys.
+      array.forEach(modifiedState => {
+        changedKeys(modifiedState);
+        addedKeys(modifiedState);
+        removedKeys(modifiedState);
+      });
+
+      added.forEach(addition => {
+        workingState = { ...workingState, ...addition };
+      });
+
+      removed.forEach(key => {
+        const { [key]: value, ...withoutKey } = workingState;
+        workingState = { ...withoutKey };
+      });
+
+      // Iterate through changed keys, recursively, and combine all changes.
+      Object.keys(workingState).reduce((prevState, key) => {
+        const prevVal = prevState[key];
+        let nextVal;
+
+        changed.forEach(changed => {
+          const original = workingState[key];
+          const newVal = changed[key];
+          const isArr = isArray(newVal);
+          const isObj = isObject(newVal);
+
+          if (isArr) {
+            const sameLength = nextVal?.length === newVal.length;
+            const shorter = nextVal?.length < newVal.length;
+
+            // console.log(nextVal || original, newVal);
+
+            // Same length? Replace with updated array.
+            if (sameLength && !isEqual(original, newVal)) {
+              return nextVal = newVal;
+            }
+
+            // Less indices than newVal? Find newly added index values.
+            if (shorter) {
+              const additions = newVal.map((item, index) => {
+                if (index > nextVal?.length - 1) return item;
+                return null;
+              }).filter(item => item);
+
+              return nextVal = [ ...nextVal || [], ...additions ];
+            }
+
+            // More indices than newVal? Find removed indices.
+            if (nextVal && !shorter) {
+              const removed = original.map((item, index) => {
+                const location = newVal.indexOf(item);
+                if (location === -1) return item;
+                return null;
+              }).filter(item => item);
+
+              const withRemovedVals = nextVal.filter(item => {
+                if (!removed.includes(item)) return item;
+                return null;
+              }).filter(item => item);
+
+              return nextVal = withRemovedVals;
+            }
+
+            // Initial nextVal undefined.
+            return nextVal = [ ...nextVal || [], ...newVal ];
+          }
+
+
+          if (isObj) {
+            // console.log(original, newVal);
+            return nextVal = { ...nextVal || {}, ...newVal };
+          }
+
+          // Initial newVal undefined.
+          return (nextVal = newVal || prevVal);
+        });
+
+        console.log({ ...prevState, [key]: nextVal });
+
+        if (nextVal) return { ...prevState, [key]: nextVal };
+        return { ...prevState };
+      }, workingState);
+    }
+
+    return workingState;
   };
 
   get(stateKey, stringOrIndex) {
