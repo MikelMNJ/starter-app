80a3e60f0616daab17b9a8cb893fc378b187acc9
Mikel Jagan <mikelmnj@gmail.com>
2022-04-02 09:44:18 -0400

    Extracted routes to separate front-end route controller.

---
+++ src/controllers/routesController.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Route, Navigate } from 'react-router-dom';
+import AuthRoute from 'components/AuthRoute/AuthRoute';
+import NotFound from 'components/NotFound/NotFound';
+import DeleteMe from 'scenes/DeleteMe/DeleteMe';
+
+const routes = [
+  {
+    // 404 Example
+    path: "*",
+    element: <NotFound />,
+  },
+  {
+    path: "/login",
+    element: <p>Log in</p>,
+  },
+  {
+    path: "/ready",
+    element: <DeleteMe />,
+  },
+  {
+    // Private route example
+    path: "/authenticated-route",
+    element: <p>Authenticated Content</p>,
+    authenticate: true,
+  },
+  {
+    // Redirect example
+    path: "/",
+    element: <Navigate to="/ready" />,
+  },
+];
+
+const makeRoutes = authToken => {
+  return routes.map((route, index) => {
+    const { path, element, authenticate } = route;
+
+    if (authenticate) {
+      return (
+        <Route key={index} element={<AuthRoute auth={authToken} />}>
+          <Route path={path} element={element} />
+        </Route>
+      );
+    }
+
+    return <Route key={index} path={path} element={element} />;
+  });
+};
+
+export default makeRoutes;
